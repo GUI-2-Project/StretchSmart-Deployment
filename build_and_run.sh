53fe304b5c438fe83d373d8611b3b4afec3a9876
Void <edward_alderman@student.uml.edu>
2024-07-27 18:42:36 +0000

    Restructure to ommit source code from deployment repo

--- build_and_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]
-    then
-        echo "Requires one argument: 'backend' or 'frontend'"
-    else
-        docker compose run --rm --build --remove-orphans --service-ports $1
-fi
